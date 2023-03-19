1d35bae6720211497cd0e568b0e93763fabd7312
chep <vcheplev42@gmail.com>
2023-03-19 13:52:54 +0400

    fix idiot bug

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2d0838682ac7aa40"; }
+/******/ 		__webpack_require__.h = function() { return "55ec35ccbb319694"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
